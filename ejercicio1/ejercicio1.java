2b761bff73869cb3f70272f26261e2ab361f15ce
169943alejandroroque <46771407+alejandroroque@users.noreply.github.com>
2019-02-07 13:49:04 -0700

    Add files via upload

---
+++ ejercicio1/ejercicio1.java
@@ -0,0 +1,9 @@
+// Nombre: Jose Alejandro Roque 
+// Dia: 07/02/2019
+// Hora: 12:56
+public class ejercicio1{
+  public static void main(String args[]){
+   System.out.println("Hola mundo"); // esta funcion se encarga de imprimir el texto indicado entre comillas
+   System.out.println("Alejandro Roque"); //esta funcion se encarga de imprimir el texto indicado entre comillas
+ }
+}
