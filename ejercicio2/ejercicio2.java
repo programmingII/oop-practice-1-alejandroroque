2fbbb10226a345c987618bd435e6881c37090cdc
169943alejandroroque <46771407+alejandroroque@users.noreply.github.com>
2019-02-07 13:47:22 -0700

    Add files via upload

---
+++ ejercicio2/ejercicio2.java
@@ -0,0 +1,13 @@
+// Nombre:Jose Alejandro Roque 
+// Dia: 07/02/2019
+// Hora:1:01 
+
+public class ejercicio2{
+  public static void main(String args[]){
+  int x= 74; // se declara la variable int x indicando que es igual a 74
+  int y= 36; // se declara la variable int y indicando que es igual a 36
+  int resultado=0 ;  // se declara la variable int resultado que es igual a 0
+  resultado= x+y;  // se genera la suma "x" y "y" para obtener el resultado
+  System.out.println("La suma de 74+36 es:"+resultado);  // aqui se declara el texto en donde se definira el resultado de las variables declaradas y la suma de estas 
+ }
+}
