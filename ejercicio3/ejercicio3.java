a44077cf41b9e6b07268a0f48795de5edfea7db9
169943alejandroroque <46771407+alejandroroque@users.noreply.github.com>
2019-02-07 13:47:57 -0700

    Add files via upload

---
+++ ejercicio3/ejercicio3.java
@@ -0,0 +1,13 @@
+// Nombre: Jose Alejandro Roque 
+// Dia: 07/02/2019
+// Hora: 1:15
+
+public class  ejercicio3{
+public static void main(String args[]){
+  int x=50;  // se declara la variable entero "x" dando un valor de 50
+  int y=3;   // se declara la variable entero "y" dando un valor de 3
+  int resultado=0;  // se declara la variable entero "resultado" dando un valor de 0
+  resultado=x/y;   // se genrera la operacion pedida que es "x" / "y"
+  System.out.println("La division de 50/3 es de:"+resultado);  // se ejecuta el comando en donde se imprimira en pantalla el resultado de la operacion
+ }
+}
---
+++ ejercicio4/ejercicio4.java
@@ -0,0 +1,12 @@
+// Nombre: Jose Alejandro Roque 
+// Dia: 07/02/2019
+// Hora: 1:22
+
+public class ejercicio4{
+public static void main(String args[]){
+  System.out.println(-5 + 8 * 6); // se ejecuta el codigo imprimir y al mismo tiempo se ejecuta la operacion deseada
+  System.out.println((55+9) % 9);  // se ejecuta el codigo imprimir y a la vez se ejecuta la operacion ingresada
+  System.out.println(20 + -3*5 / 8); // Se ejecuta la linea de imprimir y al mismo tiempo se ejecuta la operacion ingresada 
+  System.out.println(5 + 15 / 3 * 2 - 8 % 3);  // Se ejecuta la linea de imprimir y a la vez se ejecuta la operacion ingresada
+ }
+}
