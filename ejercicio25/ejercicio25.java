f5a2b792f27ac4dde4c2bafd60191d799969b372
169943alejandroroque <46771407+alejandroroque@users.noreply.github.com>
2019-02-13 22:11:30 -0700

    Add files via upload

---
+++ ejercicio25/ejercicio25.java
@@ -0,0 +1,20 @@
+// Nombre: Jose Alejandro Roque 
+// Dia: 13/02/2019
+// Hora: 9:59pm
+
+import java.util.Scanner; //la parte import java sirve para agregar una libreria a nuestro programa
+// y la palabra Sacanner es una sub libreria que a nosotros nos permite introducir datos desde el teclado hacia el programa
+
+public class ejercicio25{
+  public static void main(String args[]){
+  
+  Scanner scan = new Scanner(System.in); // la palabra Scanner se nesesita para el objeto, la palabra in es por el nombre , luego la parte de " = new" es para inicializarlo 
+ // por otra parte , lo del parentesis la palabra in es indicar que va a recibir datos
+  
+
+  System.out.print("Ingrese el numero Octal para convertirlo a Decimal: "); // se imprime en pantalla un frase indicando que ingrese el numero para convertirlo a Decimal
+  String a = scan.nextLine(); // lee el numero ingresado por el usuario a la variable a
+  System.out.println("El numero Decimal es: " + ((Integer.parseInt(a, 8))));  // esta linea lo que hace es que toma el valor ingresado de la variable a para que en si
+   // el numero ingresado se transforme a Decimal usando el Integer.pareInt
+ }
+}
