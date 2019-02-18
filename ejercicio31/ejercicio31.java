5ac6ced8968ffa5a3c1f8df5fc6ddb6acdde30dd
169943alejandroroque <46771407+alejandroroque@users.noreply.github.com>
2019-02-18 12:05:12 -0700

    Add files via upload

---
+++ ejercicio31/ejercicio31.java
@@ -0,0 +1,15 @@
+// Nombre: Jose Alejandro Roque
+// Dia: 14/02/2019
+// Hora: 1:15pm 
+
+
+ public class ejercicio31 {
+  public static void main(String[] args) {
+    System.out.println("\n Version de Java: "+System.getProperty("java.version")); 
+    System.out.println("Version de Java Runtime: "+System.getProperty("java.runtime.version"));
+    System.out.println("Pagina de inicio: "+System.getProperty("java.home"));
+    System.out.println("Proveedor Java: "+System.getProperty("java.vendor"));
+    System.out.println("Java URL Proveedor: "+System.getProperty("java.vendor.url"));
+    System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n");
+ }
+}
---
+++ ejercicio32/ejercicio32.java
@@ -0,0 +1,34 @@
+// Nombre: Jose Alejandro Roque
+// Dia: 16/02/2019
+// Hora: 1:17pm 
+
+import java.util.Scanner;
+public class ejercicio32 {
+ public static void main( String args[] )    
+    {
+        // create Scanner to obtain input from command window       
+        Scanner input = new Scanner(System.in);
+        int number1; // first number to compare       
+        int number2; // second number to compare  
+      
+        System.out.print( "Ingrese el primer numero: " ); // prompt        
+        number1 = input.nextInt(); // read first number from user   
+ 
+        System.out.print( "Ingrese el segundo numero: " ); // prompt        
+        number2 = input.nextInt(); // read second number from user               
+        
+        if ( number1 == number2 )           
+            System.out.printf( "%d == %d\n", number1, number2 );  
+        if ( number1 != number2 )          
+            System.out.printf( "%d != %d\n", number1, number2 );  
+        if ( number1 < number2 )          
+            System.out.printf( "%d < %d\n", number1, number2 );  
+        if ( number1 > number2 )          
+            System.out.printf( "%d > %d\n", number1, number2 );  
+        if ( number1 <= number2 )          
+            System.out.printf( "%d <= %d\n", number1, number2 );  
+        if ( number1 >= number2 )          
+            System.out.printf( "%d >= %d\n", number1, number2 );  
+    }
+  }
+  
