87685dfe1e0e6eb1ad608100782ad9d535e5a006
169943alejandroroque <46771407+alejandroroque@users.noreply.github.com>
2019-03-07 10:09:48 -0700

    Add files via upload

---
+++ ejercicio73/ejercicio73.java
@@ -0,0 +1,19 @@
+// Nombre: Jose Alejandro Roque Leyva
+// Fecha: 6/03/2019
+// Hora: 7:38pm
+
+
+public class ejercicio73{
+ public static void main(String []args){
+ 
+ String str1="Python"; // Declaracion de la variable str1 indicando que es String
+ String str2= "Tutorial"; // Declaracion de la variable str2 indicando que es String
+
+ 
+
+ System.out.println("ES:"+ str1.charAt(0) + str2.charAt(7));
+ // Se imprime en pantalla la palabra "ES" seguido con la funcion charAt
+ // indicando que devolvera un valor con la posicicion indicada en la linea
+ 
+ }
+}
---
+++ ejercicio74/ejercicio74.java
@@ -0,0 +1,12 @@
+// Nombre: Jose Alejandro Roque
+// Dia: 07/03/2019
+// Hora: 2:24am
+
+public class ejercicio74{
+ public static void main(String [] args){
+ 
+ int[] num_array= {10,-20,0,30,40,60,10}; // declaracion del arreglo como num_array con 7 valores
+ System.out.println((num_array[0] ==10 || num_array[num_array.length-1] ==10));
+  // se imprime en pantalla si se cumple la condicion de que si el la posicion 0 del arreglo es igual a 10, mandara true
+ }
+}
